8bed6dd9359aafda300c98c264f5527a6e5b7617
Sotiris Ntokas <ntokas1995@live.com>
2026-03-04 20:01:23 +0100

    Created the functionality to show books

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
-function Book(title, author, pages, haveRead) {
+const myLibrary = [];
+
+function Book(id, title, author, pages, haveRead) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.haveRead = haveRead;
-  this.info = function () {
-    console.log(
-      `${this.title} by ${this.author}, ${this.pages} pages, that for now I ${this.haveRead}, has a sale and I'm going to buy it ASAP!`
-    );
-  };
+  
 }
 
 const firstBook = new Book(
+    1,
   'O trofimos',
   'Emilios Gkletsos',
   420,
   "haven't read"
 );
 
-const secondBoek = new Book('PEZOS', 'Afoi gkagkatsi', 890, 'have read');
 
-Object.getPrototypeOf(firstBook) === Book.prototype;
-Object.getPrototypeOf(secondBoek) === Book.prototype;
 
-Book.prototype.givePrice = function () {
-  console.log('Hello I am so expensive!');
-};
+function addBookToLibrary(title, author, pages, haveRead){
+    const id = crypto.randomUUID();
+    const newBook = new Book (id, title, author, pages, haveRead);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}
 
-firstBook.givePrice();
-secondBoek.givePrice();
+addBookToLibrary('O trofimos', 'Emilios Gkletsos', 420, "haven't read");
+addBookToLibrary('PEZOS', 'Afoi gkagkatsi', 890, 'have read');
+addBookToLibrary('Paralitos', 'Alfonso Davies', 569, 'have not read');
+addBookToLibrary('TestoSteroni', 'Petra Petra', 1000, 'have read');
+addBookToLibrary('Amore mio', 'Evi Rountou', 6969, 'have not read');
 
-Object.getPrototypeOf(Book.prototype) === Object.prototype;
+function displayBooks(arr){
+    const bookList = document.querySelector('.book-list');  
+    bookList.innerHTML = '';
 
-firstBook.valueOf();
+    arr.forEach(book => {
+        const li = document.createElement('li');
+        li.textContent = `${book.title.toUpperCase()} by ${book.author}, ${book.pages}, ${book.haveRead}`;
+        bookList.appendChild(li);
+    })
+}
+
+displayBooks(myLibrary);
