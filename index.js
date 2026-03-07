e401fd6db68bf3b99c153b2f568cf6e5aba65408
Sotiris Ntokas <ntokas1995@live.com>
2026-03-07 11:30:44 +0100

    Experimented with tailwind

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function addBookToLibrary(title, author, pages, haveRead) {
     const id = crypto.randomUUID();
     const newBook = new Book(id, title, author, pages, haveRead);
     myLibrary.push(newBook);
-    console.log(myLibrary);
+    console.table(myLibrary);
 }
 
 addBookToLibrary('O trofimos', 'Emilios Gkletsos', 420, "haven't read");
@@ -32,7 +32,7 @@ addBookToLibrary('Paralitos', 'Alfonso Davies', 569, 'have not read');
 addBookToLibrary('TestoSteroni', 'Petra Petra', 1000, 'have read');
 addBookToLibrary('Amore mio', 'Evi Rountou', 6969, 'have not read');
 
-Book.prototype.toggleRead = function() {
+Book.prototype.toggleRead = function () {
     if (this.haveRead === "Have read") {
         this.haveRead = "Haven't read";
     } else {
@@ -49,17 +49,17 @@ function displayBooks(arr) {
     arr.forEach(book => {
         const li = document.createElement('li');
         li.textContent = `${book.title.toUpperCase()} by ${book.author}, ${book.pages}, ${book.haveRead}. `;
-        li.style.cssText = `
-         background-color: #f0f0f0;  
-  border: 1px solid #ccc;     
-  border-radius: 8px;          
-  margin: 10px 0;
-  box-shadow: 2px 2px 5px rgba(0,0,0,0.1); 
-  display: flex;
-  justify-content: space-between; 
-  align-items: center;
-  font-family: Arial, sans-serif;
-        `;
+//         li.style.cssText = `
+//          background-color: #f0f0f0;  
+//   border: 1px solid #ccc;     
+//   border-radius: 8px;          
+//   margin: 10px 0;
+//   box-shadow: 2px 2px 5px rgba(0,0,0,0.1); 
+//   display: flex;
+//   justify-content: space-between; 
+//   align-items: center;
+//   font-family: Arial, sans-serif;
+//         `;
         bookList.appendChild(li);
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = "Delete";
@@ -89,15 +89,15 @@ function displayBooks(arr) {
         //Button to toggle the have read option
         const toggleBtn = document.createElement('button');
         toggleBtn.textContent = "Toggle Read";
-        toggleBtn.style.cssText = `
-  background-color: #2196F3; 
-  color: white;
-  padding: 5px 10px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin-left: 10px;
-`;
+//         toggleBtn.style.cssText = `
+//   background-color: #2196F3; 
+//   color: white;
+//   padding: 5px 10px;
+//   border: none;
+//   border-radius: 4px;
+//   cursor: pointer;
+//   margin-left: 10px;
+// `;
 
         li.appendChild(toggleBtn);
 
@@ -115,61 +115,62 @@ displayBooks(myLibrary);
 const container = document.querySelector("#card")
 const newBookBtn = document.createElement("button");
 newBookBtn.textContent = "Create new Book";
-newBookBtn.style.cssText = `
- background-color: #4CAF50; 
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.2s ease;
-`;
+// newBookBtn.style.cssText = `
+//  background-color: #4CAF50; 
+//   color: white;
+//   padding: 10px 20px;
+//   border: none;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   transition: background-color 0.2s ease;
+// `;
 
 container.appendChild(newBookBtn);
 
 const form = document.getElementById("book-form");
 form.style.display = 'none';
 
-form.style.cssText += `      
-  width: 450px;          
-  max-width: 90%;         
-  margin: 30px auto;      
-  padding: 30px;          
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  background-color: #f9f9f9;
-  box-shadow: 3px 3px 12px rgba(0,0,0,0.15);
-  font-family: Arial, sans-serif;
-  font-size: 16px;
-`;
+// form.style.cssText += `      
+//   width: 450px;          
+//   max-width: 90%;         
+//   margin: 30px auto;      
+//   padding: 30px;          
+//   border: 1px solid #ccc;
+//   border-radius: 10px;
+//   background-color: #f9f9f9;
+//   box-shadow: 3px 3px 12px rgba(0,0,0,0.15);
+//   font-family: Arial, sans-serif;
+//   font-size: 16px;
+// `;
 newBookBtn.addEventListener("click", () => {
     form.style.display = 'block';
 
 });
 
-const inputs = form.querySelectorAll('input[type="text"], input[type="number"]');
-inputs.forEach(input => {
-    input.style.cssText = `
-    width: 100%;
-    padding: 12px;
-    margin-bottom: 15px;
-    border-radius: 6px;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-    font-size: 16px;
-  `;
+newBookBtn.addEventListener("mouseover", () => {
+    newBookBtn.style.backgroundColor = "#ab0d83";
 });
 
-const addBookBtn = document.querySelector(".add-book");
-addBookBtn.style.cssText = `
-background-color: #4CAF50; 
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.2s ease;`
-    ;
+newBookBtn.addEventListener("mouseout", () => {
+    newBookBtn.style.backgroundColor = "#4CAF50";
+});
+
+const inputs = form.querySelectorAll('input[type="text"], input[type="number"]');
+// inputs.forEach(input => {
+//     input.style.cssText = `
+//     width: 100%;
+//     padding: 12px;
+//     margin-bottom: 15px;
+//     border-radius: 6px;
+//     border: 1px solid #ccc;
+//     box-sizing: border-box;
+//     font-size: 16px;
+//   `;
+// });
+
+const addBookBtn = document.querySelector("#add-book");
+addBookBtn.classList.add("add-book-btn");
+
 
 form.appendChild(addBookBtn);
 form.addEventListener('submit', (event) => {
@@ -185,7 +186,7 @@ form.addEventListener('submit', (event) => {
     displayBooks(myLibrary);
     form.style.display = 'none';
     form.reset();
-    
+
 })
 
 
